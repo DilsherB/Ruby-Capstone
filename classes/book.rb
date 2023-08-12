4b43dc6b5a6fc77cc9bc081a7d4afa769ba3b2ac
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-08-12 20:01:40 +0300

    Addition of book class

---
+++ classes/book.rb
@@ -0,0 +1,15 @@
+require_relative 'items'
+
+class Book < Items
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publisher, cover_state)
+    super()
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super() || cover_state == 'bad'
+  end
+end
--- main.rb
+++ main.rb
@@ -1,34 +1,35 @@
 def options_list
   options = {
-    1 => "List All Books",
-    2 => "List All Music Albums",
-    3 => "List All Games",
-    4 => "List All Geners",
-    5 => "List All Labels",
-    6 => "List All Authors",
-    7 => "Add A Book",
-    8 => "Add A Music Album",
-    9 => "Add A Game",
-    0 => "Exit"
+    1 => 'List All Books',
+    2 => 'List All Music Albums',
+    3 => 'List All Games',
+    4 => 'List All Geners',
+    5 => 'List All Labels',
+    6 => 'List All Authors',
+    7 => 'Add A Book',
+    8 => 'Add A Music Album',
+    9 => 'Add A Game',
+    0 => 'Exit'
   }
 
-  options.each {|key, option| puts "#{key} - #{option}"}
+  options.each { |key, option| puts "#{key} - #{option}" }
   gets.chomp
 end
-  def main
-    puts 'Welcome to Catalog of my things!'
-    number = options_list
-  
-    loop do
-      case number
-      when '0'
-        puts 'Thank you for using this app Goodbye!'
-        exit
-      else
-        puts 'Invalid input'
-        break
-      end
+
+def main
+  puts 'Welcome to Catalog of my things!'
+  number = options_list
+
+  loop do
+    case number
+    when '0'
+      puts 'Thank you for using this app Goodbye!'
+      exit
+    else
+      puts 'Invalid input'
+      break
     end
   end
-  
-  main
+end
+
+main
